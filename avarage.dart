68f38eaf10bf191395d2b79b2299d7e5d8c5f380
Ian Jarvis <ijarvis12@gmail.com>
2022-07-13 09:03:43 -0500

    Ending print statmnt

--- avarage.dart
+++ avarage.dart
@@ -12,5 +12,6 @@ void main() {
         }
         double? average = total / i;
         print("The average: $average");
+        print("Press <Enter> to exit");
         String? _ = stdin.readLineSync()!;
 }
