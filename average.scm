2db0321add37bf8eb9e9af36dfd86c9a75909bd7
Ian Jarvis <ijarvis12@gmail.com>
2024-04-10 12:03:00 -0500

    Create average.scm

---
+++ average.scm
@@ -0,0 +1,36 @@
+#!/home/ijarvis12/.guix-profile/bin/guile -s
+!#
+
+;; Program finds the average of numerical inputs
+
+;; Use module for text i/o
+(use-modules (ice-9 textual-ports))
+
+(display "This program takes input(s) and finds the average")
+(newline)
+
+;; Variable that holds the total of the inputs
+(define total 0)
+
+;; Variable that holds the number of inputs
+(define i 0)
+
+(display "Input a number (<Enter> to exit)")
+(newline)
+
+;; Variable for input
+(define x "")
+
+(while (> 1 0)
+        (set! x (get-line (current-input-port)))
+        (if (= 0 (string-length x)) (break))
+        (if (not (number? (string->number x))) (break))
+        (set! total (+ (string->number x) total))
+        (set! i (+ i 1))
+)
+
+(display (string-append "Average: " (number->string (/ total i))))
+(newline)
+
+(display "Press <Enter> to end program")
+(get-line (current-input-port))
